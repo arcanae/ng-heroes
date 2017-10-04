40f5b3ce90691f7ee181bb113f74e542a058cd81
Arcanae <justin.courdesse@gmail.com>
2017-10-04 09:21:01 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ListHeroesComponent } from './list-heroes/list-heroes.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroesService } from './heroes.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListHeroesComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
