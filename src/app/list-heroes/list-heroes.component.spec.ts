e5ea8d418efce681027591e8e00fce567e73b9e5
Arcanae <justin.courdesse@gmail.com>
2017-10-09 14:05:24 +0200

    up

--- src/app/list-heroes/list-heroes.component.spec.ts
+++ src/app/list-heroes/list-heroes.component.spec.ts
@@ -7,6 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/observable/of';
+import { Hero } from '../hero';
+import { By } from '@angular/platform-browser';
 
 describe('ListHeroesComponent', () => {
   let component: ListHeroesComponent;
@@ -26,12 +28,29 @@ describe('ListHeroesComponent', () => {
     fixture = TestBed.createComponent(ListHeroesComponent);
     component = fixture.componentInstance;
     let service = fixture.debugElement.injector.get(HeroesService);
-    spyOn(service, 'getAllHeroes').and.returnValue(Observable.of(['ga','zo','bu','meu']))
+    spyOn(service, 'getAllHeroes').and.returnValue(Observable.of([new Hero('Karna', 1), new Hero('Mordred', 2), new Hero('Jeanne', 3)]))
 
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create with 3 li\'s', () => {
     expect(component).toBeTruthy();
+    let lis = fixture.debugElement
+    .queryAll(By.css('li'));
+
+    expect(lis.length).toBe(3);
+  });
+
+  it('should capitalize hero name', () => {
+    let hero = new Hero('karna',1);
+    let li = fixture.debugElement
+    .query(By.css('li')).nativeElement;
+    
+    li.click();
+
+
+    // component.inspect(hero);
+    expect(li.textContent.trim()).toBe('Karna');
+
   });
 });
